24c5bdb2ec5dec1a61385cc987b9edd31d244841
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-26 15:57:59 +0200

    feat: add second track to readme

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,16 @@
 //PRIMA TRACCIA:
 
-const firstNumber = parseInt(prompt ("inserisci un numero"))
-const secondNumber = parseInt(prompt ("inserisci un altro numero"))
+//const firstNumber = parseInt(prompt ("inserisci un numero"))
+//const secondNumber = parseInt(prompt ("inserisci un altro numero"))
+//if (firstNumber < secondNumber) { 
+    //max = secondNumber
+    //min = firstNumber
+//}
+//else if(secondNumber < firstNumber) {
+   //min = secondNumber
+   //max = firstNumber
+//}
+//let randomNumber = Math.floor(Math.random() * (max - min) + min)
+//console.log(randomNumber);
 
-
-
-if (firstNumber < secondNumber) { 
-    max = secondNumber
-    min = firstNumber
-}
-else if(secondNumber < firstNumber) {
-   min = secondNumber
-   max = firstNumber
-}
-
-let randomNumber = Math.floor(Math.random() * (max - min) + min)
-console.log(randomNumber);
 //SECONDA TRACCIA:
--- readme.md
+++ readme.md
@@ -5,3 +5,16 @@ Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale com
 - chiedere all'utente un numero,
 - chiedere all'utente un altro numero,
 - generare un numero casuale che sia compreso tra i due precedenti.
+
+# seconda traccia
+# descrizione:
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+# procedimento:
+- chiedere una parola all'utente,
+- chiedere un'altra parola all'utente,
+- controllare la lunghezza delle due parole,
+- se hanno la stessa lunghezza:
+  - stampare entrambe.
+- se hanno lunghezze differenti:
+  - stampare la più lunga.  
