8e46926b60ac7542dc01000c5ad11830398a943c
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-26 15:54:22 +0200

    fix: fixed condition to first track

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,19 @@
-const firstNumber = prompt ("inserisci un numero")
-const secondNumber = prompt ("inserisci un altro numero")
+//PRIMA TRACCIA:
 
-let randomNumber = Math.floor(Math.random() * secondNumber + firstNumber)
-console.log(randomNumber)
+const firstNumber = parseInt(prompt ("inserisci un numero"))
+const secondNumber = parseInt(prompt ("inserisci un altro numero"))
+
+
+
+if (firstNumber < secondNumber) { 
+    max = secondNumber
+    min = firstNumber
+}
+else if(secondNumber < firstNumber) {
+   min = secondNumber
+   max = firstNumber
+}
+
+let randomNumber = Math.floor(Math.random() * (max - min) + min)
+console.log(randomNumber);
+//SECONDA TRACCIA:
